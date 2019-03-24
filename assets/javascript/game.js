734f0bf3da66fe8c63befeb5f788d3d406a4b3e2
KenanSultan <kenansultan@hotmail.com>
2019-03-24 15:14:59 +0400

    header width fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 var wins = 0
-var bands = ["BLONDIE","GENESIS","INXS","JOURNEY","MADONNA","METALLICA","POISON","QUEEN","RUSH","TOTO","U2"]
+var bands = ["BLONDIE", "GENESIS", "INXS", "JOURNEY", "MADONNA", "METALLICA", "POISON", "QUEEN", "RUSH", "TOTO", "U2"]
 
-for(let i=0; i<bands.length; i++){
+for (let i = 0; i < bands.length; i++) {
     console.log(bands[i])
 }
 
+// Yeni oyun baslatmak icin fonksiyon
 function new_game() {
-    band = bands[Math.floor(Math.random()*bands.length)]
+    band = bands[Math.floor(Math.random() * bands.length)]
     len = band.length
     chanses = 12
     letters = []
@@ -18,7 +19,7 @@ function new_game() {
     guess_space.innerHTML = ""
     chanses_space = document.querySelector("#chanses")
     chanses_space.innerHTML = chanses
-    for(let i=0; i<len; i++){
+    for (let i = 0; i < len; i++) {
         letters.push(band[i])
         letter_space = document.createElement('span')
         letter_space.id = i
@@ -27,58 +28,67 @@ function new_game() {
     }
 }
 
-new_game()
+// Kazaninca cagirilir
+function win_func() {
+    message = document.querySelector("#msg")
+    message.innerHTML = "YOU WIN"
+    wins++
+    let win_space = document.querySelector("#wins")
+    win_space.innerHTML = wins
+    new_game()
+}
 
-// Herhangi tusha basildiginda
-window.onkeypress = key_click
+// Kaybedince cagirilir
+function lose_func() {
+    message = document.querySelector("#msg")
+    message.innerHTML = "YOU LOSE"
+    new_game()
+}
 
+// Herhangi tusa basildiginda devreye girecek fonksiyon
 function key_click(event) {
     // girilen harfi guess olarak aldim
     var guess = String.fromCharCode(event.keyCode).toUpperCase()
     // yeni yanlish harf girildiginde
-    if(guesses.indexOf(guess)===-1 && letters.indexOf(guess)===-1){
-        if(chanses>1){   
+    if (guesses.indexOf(guess) === -1 && letters.indexOf(guess) === -1) {
+        if (chanses > 1) {
             chanses--
             guesses.push(guess)
             guess_space = document.querySelector("#guesses")
-            if(guesses.length>1){
+            if (guesses.length > 1) {
                 guess_space.innerHTML += ", " + guess
-            }else{
+            } else {
                 guess_space.innerHTML += guess
                 message = document.querySelector("#msg")
                 message.innerHTML = ""
             }
             chanses_space = document.querySelector("#chanses")
             chanses_space.innerHTML = chanses
-        }else{
-            new_game()
-            message = document.querySelector("#msg")
-            message.innerHTML = "YOU LOSE"
-            guess_space.innerHTML = ""
-            chanses_space.innerHTML = chanses     
-        } 
+        } else {
+            lose_func()
+        }
     // Dogru harf girildiginde
-    }else if(right_guesses.indexOf(guess)===-1 && letters.indexOf(guess)!==-1){
+    } else if (right_guesses.indexOf(guess) === -1 && letters.indexOf(guess) !== -1) {
         message = document.querySelector("#msg")
         message.innerHTML = ""
         right_guesses.push(guess)
-        if(len>1){
+        if (len > 1) {
             var i = 0;
-            while(letters.indexOf(guess,i)>=0) {
-                var a = letters.indexOf(guess,i)
+            while (letters.indexOf(guess, i) >= 0) {
+                var a = letters.indexOf(guess, i)
                 var span_space = document.getElementById(a)
                 span_space.innerHTML = guess
                 len--
-                i = a+1
+                i = a + 1
             }
-        }else{
-            message = document.querySelector("#msg")
-            message.innerHTML = "YOU WIN"
-            wins++
-            let win_space = document.querySelector("#wins")
-            win_space.innerHTML = wins  
-            new_game() 
+        } else {
+            win_func()
         }
     }
 }
 
+// Ilk oyunu baslat
+new_game()
+
+// Herhangi tusha basildiginda
+window.onkeypress = key_click
